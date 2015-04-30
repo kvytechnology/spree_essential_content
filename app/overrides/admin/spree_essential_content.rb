7b70d46478e19d3308855982c7cbeada5846465e
Joe S <joe@pixelbash.co.nz>
2015-04-30 00:58:58 +0000

    Started the long hard trek of converting everything to bootstrap

--- app/overrides/admin/spree_essential_content.rb
+++ app/overrides/admin/spree_essential_content.rb
@@ -1,12 +1,12 @@
 # Adds the Content button to the admin tabs
-Deface::Override.new(virtual_path:  "spree/admin/shared/_menu",
+Deface::Override.new(virtual_path:  "spree/admin/shared/_main_menu",
                      name:          "spree_essential_admin_tabs",
-                     insert_bottom: "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     insert_before: "erb:contains('Spree::Order')",
                      partial:       "spree/admin/shared/contents_tab",
                      disabled:      false)
 
 # Adds configuration links to the configurations sidebar
-Deface::Override.new(virtual_path:  "spree/admin/shared/_configuration_menu",
+Deface::Override.new(virtual_path:  "spree/admin/shared/sub_menu/_configuration",
                      name:          "add_essential_content_settings_sidebar",
                      insert_bottom: "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
                      partial:       "spree/admin/shared/blog_config",
